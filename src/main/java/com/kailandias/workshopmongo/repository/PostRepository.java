5c7475cd125d9fb6a0b5b4ceceaa1bf06f2064a7
Kailan <kailanlopes1103@gmail.com>
2025-05-04 22:07:37 -0300

    Consulta com Query

--- src/main/java/com/kailandias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/kailandias/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.kailandias.workshopmongo.repository;
 import com.kailandias.workshopmongo.domain.Post;
 import com.kailandias.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post,String> {
 
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{'title': {$regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
 }
